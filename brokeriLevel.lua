a8b829c265aebc8dfc855741a2cfa0e102276533
timothyluke <timothyluke@32238858-db82-4f3b-b8b0-e1e56237f78d>
2010-12-15 14:09:54 +0000

    Added simple tooltip character history

--- brokeriLevel.lua
+++ brokeriLevel.lua
@@ -1,19 +1,46 @@
 local UPDATEPERIOD, elapsed = 0.5, 0
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local dataobj = ldb:NewDataObject("iLevel", {type = "data source", text = "Current ilevel: 200"})
+local class, classFileName = UnitClass("player");
 local f = CreateFrame("frame")
+local name = GetUnitName("player", false);
+local iLevel = GetAverageItemLevel()
+f:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
+f:RegisterEvent("PLAYER_LOGOUT"); -- Fired when about to log out
+
 
 f:SetScript("OnUpdate", function(self, elap)
 	elapsed = elapsed + elap
 	if elapsed < UPDATEPERIOD then return end
 
 	elapsed = 0
-	local iLevel = GetAverageItemLevel()
+	iLevel = GetAverageItemLevel()
 	dataobj.text = string.format("Current ilevel: %.1f", iLevel)
+        addonLoadedBool = true
+
+        if not draiksAddonInitialised  then
+         characterclassTable = {}
+         characterilevelTable = {}
+         characterNameTable = {}
+         draiksAddonLoadedBool = false
+         draiksAddonInitialised = true
+         ConsoleAddMessage("initialised")
+        end
+        draiksAddonLoadedBool = true
+        characterNameTable[name] = name;
+        characterilevelTable[name] = iLevel
+        characterclassTable[name] = classFileName;
 end)
 
 function dataobj:OnTooltipShow()
-	self:AddLine("Average iLevel")
+  if addonLoadedBool then	
+    for key,value in pairs(characterNameTable) do
+      local color = RAID_CLASS_COLORS[characterclassTable[key]];
+      self:AddDoubleLine(characterNameTable[key], characterilevelTable[key], color.r, color.g, color.b, 255, 255, 255);
+    end
+  else	
+    self:AddLine("Loading Characters");
+  end
 end
 
 function dataobj:OnEnter()
