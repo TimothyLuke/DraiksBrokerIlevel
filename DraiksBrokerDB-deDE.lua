804c26d2aab43d138b1c8d8ccf2bbc532a3493ab
timothyluke <timothyluke@32238858-db82-4f3b-b8b0-e1e56237f78d>
2010-12-29 07:24:55 +0000

    French and German Localisation

---
+++ DraiksBrokerDB-deDE.lua
@@ -0,0 +1,42 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("DraiksBrokerDB", "deDE", true)
+
+L["Draiks Broker ILevel"] = true
+L["Display"] = "Anzeige"
+L["Specify what to display"] = true
+L["Main Settings"] = "Haupteinstellungen"
+L["Show Level"] = "Stellen Sie gleich dar"
+L["Show Character Levels"] = "Zeigen Sie Buchstaben-Niveaus"
+L["Factions and Realms"] = "Fraktionen und Realms"
+L["All Factions"] = = "Alle Fraktionen"
+L["All factions will be displayed"] = "Alle Fraktionen werden angezeigt"
+L["All Realms"] = = "Alle Realms"
+L["All realms will be displayed"] = "Alle Realms werden angezeigt"
+L["Sort Order"] = "Art-Auftrag"
+L["Sort Type"] = "Sortiertyp"
+L["Sort"] = "Sortieren"
+L["Select the sort type"] = "Wählen Sie die Art-Art vor"
+L["By Name"] = "Nach Namen"
+L["By Level"] = "Nach Farbe"
+L["By Item Level"] = true
+L["By Money"] = "Nach Geld"
+L["Sort in reverse order"] = "Sortiere in umgekehrter Reihenfolge"
+L["Use the curent sort type in reverse order"] = "Sortiert nach ausgewähltem Sortiertyp in umgekehrter Reihenfolge"
+L["Ignore Characters"] = "Ignoriere Charaktere"
+L["Hide characters from display"] = "Versteckt Charaktere"
+L["Realm"] = true
+L["Character Name"] = "Buchstaben-Name"
+L["UI"] = true
+L["Set UI options"] = "Stellen Sie UI Wahlen ein"
+L["Scale"] = "Skala"
+L["Scale the tooltip (70% to 150%)"] = "Stufen Sie das tooltip ein (70% bis 150%)"
+L["Opacity"] = "Transparenz"
+L["% opacity of the tooltip background"] = "% Opazität des tooltip Hintergrundes"
+L["Show Bars"] = "Zeigen Sie Stäbe"
+L["Display Table Rows as colored bars with white text"] = "Zeigen Sie Tabellen-Reihen als farbige Stäbe mit weißem Text an"
+L["dil"] = true
+L["draiksbrokerilevel"] = true
+L["draiksilvl"] = true
+L["draiksilevel"] = true
+L["Character iLevel Breakdown"] = "Buchstabe iLevel Zusammenbruch"
+L["Calculate Own Average iLevel"] = "Berechnen Sie, durchschnittliches iLevel zu besitzen"
+L["Calculate your own average iLevel based on what you have equiped instead of using the Blizzard Reported Average iLevel"] = "Berechnen Sie Ihr eigenes durchschnittliches iLevel, das basiert auf, was Sie ausgerüstet haben, anstatt, den Blizzard zu benutzen, der durchschnittliches iLevel berichtet wird"
